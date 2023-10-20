0015d7450198dd3143ae8d28ef6e1f0228bedde5
Maelys Boissezon <maelys.boissezon@etu.umontpellier.fr>
2023-10-21 00:10:07 +0200

    waaah c'est presque fini omg reste a faire: transition +lien

--- ressources/burgerBehaviour.js
+++ ressources/burgerBehaviour.js
@@ -1,20 +1,16 @@
 const menu = document.getElementById("menu");
 
 const burger = document.getElementById("burgerMenu");
-
+const cross = document.getElementById("#cross");
 console.log(burger);
-burger.addEventListener("click", ()=>open());
-
+burger.addEventListener("click", open);
+cross.addEventListener("click",close);
 function open(){
-    console.log("test");
-    if(menu.style.display==="none") {
-        menu.style.display="flex";
-    }else{
-        menu.style.display="none";
-    }
+    menu.style.display="flex";
+    menu.classList.toggle("open");
 }
 
 function close(){
-    if(menu.style)
-        menu.style.visibility ="hidden";
+   menu.style.display="none";
+    menu.classList.remove("open");
 }
