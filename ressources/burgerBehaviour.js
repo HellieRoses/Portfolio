7707268f3ea259408f8213b4fdb19877b7961508
Maelys Boissezon <maelys.boissezon@etu.umontpellier.fr>
2023-10-24 09:56:20 +0200

    transitions

--- ressources/burgerBehaviour.js
+++ ressources/burgerBehaviour.js
@@ -2,15 +2,18 @@ const menu = document.getElementById("menu");
 
 const burger = document.getElementById("burgerMenu");
 const cross = document.getElementById("#cross");
+
 console.log(burger);
 burger.addEventListener("click", open);
 cross.addEventListener("click",close);
 function open(){
     menu.style.display="flex";
-    menu.classList.toggle("open");
+    menu.style.animation="my_animation 0.2s ease-in";
 }
 
 function close(){
-   menu.style.display="none";
-    menu.classList.remove("open");
+
+    menu.style.animation="my_animation2 0.2s ease-in-out";
+    menu.style.display="none";
+
 }
